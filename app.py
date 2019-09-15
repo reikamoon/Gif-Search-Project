8e46177eb331b3b8a4e192c3d2f862cc9c527868
Anjelica <ambrosio.anjelica@gmail.com>
2019-09-15 14:01:35 -0700

    updated app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Return homepage."""
+
     # TODO: Extract query term from url
 
     # TODO: Make 'params' dict with query term and API key
