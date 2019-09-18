22b75235e1ce2c3664cd7d17bac635516dcc97ed
Anjelica <ambrosio.anjelica@gmail.com>
2019-09-18 16:20:35 -0700

    Merged files

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ def index():
     apikey = "KWIISY5DIB57"
     lmt = 10
     search_term = request.args.get("search")
-    content_filter = ""
+    content_filter = "high"
 
-    # TODO: Extract query term from url
+        # TODO: Extract query term from url
 
-    # TODO: Make 'params' dict with query term and API key
+        # TODO: Make 'params' dict with query term and API key
     params = {
         "query" : search_term,
         "key" : apikey,
@@ -24,28 +24,28 @@ def index():
         "content_filter" : filter
     }
 
-    # TODO: Make an API call to Tenor using the 'requests' library
-        #API CODE FOR TENOR: KWIISY5DIB57
-# set the apikey
-apikey = "KWIISY5DIB57"  # test value
+        # TODO: Make an API call to Tenor using the 'requests' library
+            #API CODE FOR TENOR: KWIISY5DIB57
+    # set the apikey
+    apikey = "KWIISY5DIB57"  # test value
 
-# get the GIF's id and search used
-shard_gifs_id = top_8gifs[0]["id"]
+    # get the GIF's id and search used
+    shard_gifs_id = top_8gifs[0]["id"]
 
-search_term = "excited"
+    search_term = "excited"
 
-r = requests.get("https://api.tenor.com/v1/search", params=params)
+    r = requests.get("https://api.tenor.com/v1/search", params=params)
 
-if r.status_code == 200:
-    pass
+    if r.status_code == 200:
+        pass
     # move on
-else:
-    pass
-    # handle error
-    #credits to Tenor for template. URL for template here: https://tenor.com/gifapi/documentation?gclid=EAIaIQobChMIw8HPp-7T5AIVdBh9Ch3Xmw2fEAAYASACEgJFEvD_BwE#quickstart
-    # TODO: Get the first 10 results from the search results
+    else:
+        pass
+        # handle error
+        #credits to Tenor for template. URL for template here: https://tenor.com/gifapi/documentation?gclid=EAIaIQobChMIw8HPp-7T5AIVdBh9Ch3Xmw2fEAAYASACEgJFEvD_BwE#quickstart
+        # TODO: Get the first 10 results from the search results
 
-    # TODO: Render the 'index.html' template, passing the gifs as a named parameter
+        # TODO: Render the 'index.html' template, passing the gifs as a named parameter
 
     return render_template("index.html")
 
@@ -79,8 +79,8 @@ def random():
     }
 
     t = requests.get("https://api.tenor.com/v1/trending_terms", params=params)
-
-    return render_template("index.html")
+    if r.status_code == 200:
+        return render_template("index.html")
 
 
 if __name__ == '__main__':
