eb3ab53e9d88133b14a771398fc558580a725113
Anjelica <ambrosio.anjelica@gmail.com>
2019-09-18 15:43:27 -0700

    updated file

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import requests
 import json
+from random import choice
 
 app = Flask(__name__)
 
@@ -8,9 +9,20 @@ app = Flask(__name__)
 def index():
     """Return homepage."""
 
+    apikey = "KWIISY5DIB57"
+    lmt = 10
+    search_term = request.args.get("search")
+    content_filter = ""
+
     # TODO: Extract query term from url
 
     # TODO: Make 'params' dict with query term and API key
+    params = {
+        "query" : search_term,
+        "key" : apikey,
+        "limit" : lmt,
+        "content_filter" : filter
+    }
 
     # TODO: Make an API call to Tenor using the 'requests' library
         #API CODE FOR TENOR: KWIISY5DIB57
@@ -22,8 +34,7 @@ shard_gifs_id = top_8gifs[0]["id"]
 
 search_term = "excited"
 
-r = requests.get("https://api.tenor.com/v1/registershare?id=%s&key=%s&q=%s" % (
-shard_gifs_id, apikey, search_term))
+r = requests.get("https://api.tenor.com/v1/search", params=params)
 
 if r.status_code == 200:
     pass
@@ -37,6 +48,38 @@ else:
     # TODO: Render the 'index.html' template, passing the gifs as a named parameter
 
     return render_template("index.html")
+
+@app.route('/trending')
+def trending():
+    """Return Trednging Gifs"""
+    apikey = "KWIISY5DIB57"
+    limit = 10
+    content_filter = "high"
+
+    params = {
+        'key': apikey,
+        'limit': limit,
+        'content_filter': filter
+    }
+    r = requests.get("https://api.tenor.com/v1/trending", params=params)
+    if r.status_code == 200:
+    return render_template("index.html")
+
+@app.route('/random')
+def random():
+    """Give random gifs"""
+    apikey = "KWIISY5DIB57"
+    limit = 10
+    content_filter = "high"
+
+    params = {
+        'key': apikey,
+        'limit': limit,
+        'content_filter': filter
+    }
+
+    t = requests.get("https://api.tenor.com/v1/trending_terms", params=params)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
